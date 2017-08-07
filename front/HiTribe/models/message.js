a63734beb52ae54175a128d025bd52670eb4e9a3
asambvani <alexsambvani@gmail.com>
2017-08-07 12:54:41 -0400

    Small front end change

--- front/HiTribe/models/message.js
+++ front/HiTribe/models/message.js
@@ -3,7 +3,7 @@ function createMessageClass(){
   return class{
     static commentsHTML(message){
       return this.comments(message).map(function(comment){
-        return `<div class="comment card-action"><strong>${User.find(comment.user_id).username}</strong>: ${comment.comment_text}</div>`
+        return `<div class="col s12"><div class="comment card-action"><strong>${User.find(comment.user_id).username}</strong>: ${comment.comment_text}</div></div>`
       }).join("")
     }
 
